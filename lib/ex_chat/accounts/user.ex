9c0a18aa86ac4cd5827e8dcd02a8b807d1b2d4cc
Marvin Beckers <mail@embik.me>
2017-08-01 01:53:23 +0200

    Use virtual 'password' field to generate password hash from

--- lib/ex_chat/accounts/user.ex
+++ lib/ex_chat/accounts/user.ex
@@ -8,6 +8,7 @@ defmodule ExChat.Accounts.User do
     field :display_name, :string
     field :name, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -15,8 +16,15 @@ defmodule ExChat.Accounts.User do
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:name, :display_name, :password_hash])
-    |> validate_required([:name, :display_name, :password_hash])
+    |> cast(attrs, [:name, :display_name, :password])
+    |> validate_required([:name, :display_name, :password])
     |> unique_constraint(:name)
+    |> put_pass_hash
   end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true,
+      changes: %{password: password}} = changeset) do
+    change(changeset, Comeonin.Bcrypt.add_hash(password))
+  end
+  defp put_pass_hash(changeset), do: changeset
 end
